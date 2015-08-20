bc17f07007beb717528cb769ea7791d73d5c0964
Todd Huss <thuss@gabrito.com>
2015-08-20 12:21:19 -0700

    Use UIAlertView on iOS 7

--- LeakInspectorAlertProvider.swift
+++ LeakInspectorAlertProvider.swift
@@ -1,17 +1,28 @@
 @objc class LeakInspectorAlertProvider: NSObject, LeakInspectorDelegate {
 
     private weak var alertController: UIAlertController?
+    private weak var alertView: UIAlertView?
 
     func didLeakReference(ref: AnyObject, name: String) {
         if let alertController = self.alertController {
             alertController.dismissViewControllerAnimated(false, completion: nil)
+        } else if let alertView = self.alertView {
+            alertView.dismissWithClickedButtonIndex(0, animated: false)
         }
 
+        let title = "Leak Inspector"
         let message = "Detected possible leak of \(name)"
-        let alertController = UIAlertController(title: "Leak Inspector", message: message, preferredStyle: .Alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        alertController.show()
-        self.alertController = alertController
+        let ok = "OK"
+        if objc_getClass("UIAlertController".UTF8String) != nil {
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: ok, style: UIAlertActionStyle.Default, handler: nil))
+            alertController.show()
+            self.alertController = alertController
+        } else {
+            let alertView = UIAlertView(title: title, message: message, delegate: nil, cancelButtonTitle: ok)
+            alertView.show()
+            self.alertView = alertView
+        }
     }
 
 }
